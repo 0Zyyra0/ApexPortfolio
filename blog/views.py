4e070c2d2301e2f01f86b64723891c1d81be3cc0
0Zyyra0 <am49733794@gmail.com>
2026-08-25 21:37:47 +0330

    1.3.1
    Finish session 8 Maktabkhone

--- blog/views.py
+++ blog/views.py
@@ -3,19 +3,36 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post, category as Category
 
 
-def blog_view(request,cat_name=None,author_username=None):
-    posts = Post.objects.filter(status=1)
-    if cat_name:    
-        posts = Post.objects.filter(category__name=cat_name)
-    if author_username:
-        posts = posts.filter(author__username = author_username)
-    context = {'posts': posts}
+def blog_view(request, cat_name=None, author_username=None):
 
-    return render(request,'blog/blog-home.html',context)
+    posts = Post.objects.filter(status=1)
+
+    if cat_name:
+        posts = posts.filter(category__name__iexact=cat_name)
+
+    if author_username:
+        posts = posts.filter(author__username=author_username)
+
+    paginator = Paginator(posts, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'posts': page_obj,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'blog/blog-home.html', context)
 
 
 def blog_single(request, pid):
-    post = get_object_or_404(Post, pk=pid, status=1)
+
+    post = get_object_or_404(
+        Post,
+        pk=pid,
+        status=1
+    )
 
     context = {
         'post': post
@@ -27,24 +44,45 @@ def blog_single(request, pid):
 def test(request):
     return render(request, 'test.html')
 
+
 def blog_category(request, cat_name):
-    posts = Post.objects.filter(category__name__iexact=cat_name,status=1)
-    context = {'posts': posts,}
-    return render(request, 'blog/blog-home.html', context)
 
+    posts = Post.objects.filter(
+        category__name__iexact=cat_name,
+        status=1
+    )
 
-def blog_search(request):
-    query = request.GET.get('s')
+    paginator = Paginator(posts, 3)
 
-    posts = Post.objects.all()
-
-    if query:
-        posts = posts.filter(title__icontains=query)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'posts': posts,
+        'posts': page_obj,
+        'page_obj': page_obj,
     }
 
     return render(request, 'blog/blog-home.html', context)
 
-    
+
+def blog_search(request):
+
+    query = request.GET.get('s')
+
+    posts = Post.objects.filter(status=1)
+
+    if query:
+        posts = posts.filter(title__icontains=query)
+
+    paginator = Paginator(posts, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'posts': page_obj,
+        'page_obj': page_obj,
+        'query': query,
+    }
+
+    return render(request, 'blog/blog-home.html', context)
